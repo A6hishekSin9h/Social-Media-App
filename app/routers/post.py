f13210669150effd24c4d7eb15a51bc5857eed15
A6hishekSin9h <herwi6pota9@gmail.com>
2023-06-23 19:12:33 +0530

    User authentication

--- app/routers/post.py
+++ app/routers/post.py
@@ -3,17 +3,19 @@ from sqlalchemy.orm import Session
 from .. import models, schemas
 from ..database import get_db
 from typing import List
+from .. import oauth2
 
 
 router = APIRouter(
-    prefix= "/posts",
+    prefix="/posts",
     tags= ["Posts"]
 )
 
 
 
 @router.get("/", response_model=List[schemas.Post])
-def get_posts(db: Session = Depends(get_db)):
+def get_posts(db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts""")
     # posts = cursor.fetchall()
     post = db.query(models.Post).all()
@@ -21,11 +23,13 @@ def get_posts(db: Session = Depends(get_db)):
 
 
 @router.post("/", status_code = status.HTTP_201_CREATED,response_model=schemas.Post)
-def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""INSERT INTO posts (title, content, published) VALUES (%s,%s,%s) RETURNING * """
     #                ,(post.title,post.content,post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
+    print(current_user.email)
     new_post = models.Post(**post.dict())
     db.add(new_post)
     db.commit()
@@ -33,9 +37,12 @@ def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
     return new_post
 
 @router.get("/{id}",response_model=schemas.Post)
-def get_post(id:int, db: Session = Depends(get_db)):
+def get_post(id:int, db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""SELECT * FROM posts WHERE id = %s """,(str(id)))
     # post = cursor.fetchone()
+    
+
     post= db.query(models.Post).filter(models.Post.id==  id).first()
     if not post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
@@ -45,11 +52,13 @@ def get_post(id:int, db: Session = Depends(get_db)):
 
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
-def delete_post(id:int, db: Session = Depends(get_db)):
+def delete_post(id:int, db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""DELETE FROM posts WHERE id = %s RETURNING *""", (str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
     post = db.query(models.Post).filter(models.Post.id== id)
+    
 
     if post.first()==None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
@@ -60,11 +69,13 @@ def delete_post(id:int, db: Session = Depends(get_db)):
 
 
 @router.put("/{id}",response_model=schemas.Post)
-def update_post(id:int,updated_post:schemas.PostCreate, db: Session = Depends(get_db)):
+def update_post(id:int,updated_post:schemas.PostCreate, db: Session = Depends(get_db), 
+                 current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published= %s WHERE id = %s RETURNING *"""
     #                ,(post.title,post.content, post.published, str(id)))
     # updated_post = cursor.fetchone()
     # conn.commit()
+    
     post_query= db.query(models.Post).filter(models.Post.id== id)
     post=post_query.first()
     if post==None:
